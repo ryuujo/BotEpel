3f65aab6b4d5b4e4cbf71c4cf25f315ef36ad491
seisochan <80140173+seisochan@users.noreply.github.com>
2022-04-06 12:24:29 +0700

    Implementing latency feature

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,9 @@ module.exports = {
   name: "ping",
   description: "Ping me if you or this bot feel laggy",
   execute(message, args) {
-    message.channel.send('**Pong.** Aku di sini~\nLatency is ${Date.now() - message.createdTimestamp}ms');
+    var yourping = new Date().getTime() - message.createdTimestamp
+    var apiping = Math.round(client.ws.ping)
+    var botping = Math.round(bot.ws.ping)
+    message.channel.send('**Pong.** Aku di sini~\nLatency is ${yourping}ms\nAPI Latency is ${apiping}ms\nBot Latency is ${botping}ms');
   }
 };
