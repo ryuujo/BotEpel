0ca97791d724958644dbd3bb54b6f9c882076a04
seisochan <80140173+seisochan@users.noreply.github.com>
2022-04-06 12:54:48 +0700

    Revert

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,6 @@ module.exports = {
   name: "ping",
   description: "Ping me if you or this bot feel laggy",
   execute(message, args) {
-    const ping = resultMessage.createdTimestamp - message.createdTimestamp
     message.channel.send('**Pong.** Aku di sini~');
   }
 };
